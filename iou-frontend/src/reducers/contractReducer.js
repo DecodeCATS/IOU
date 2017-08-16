421c247e468e57cd9bb957b24c88e84ceba1c4e7
Tetyana Myronova <tetyanamyronova@LWCHAmyronovate.local>
2017-08-16 17:32:09 -0400

    Add contractReducer... testing

--- iou-frontend/src/reducers/contractReducer.js
+++ iou-frontend/src/reducers/contractReducer.js
@@ -1,4 +1,10 @@
+import {applyMiddleware, createStore} from 'redux';
+import axios from 'axios';
+
+// import config from '../config'
+
 //Reducer acting on the contracts
+
 
 //The reducer affecting the contract
 const contractReducer = (state=[], action) => {
@@ -8,5 +14,18 @@ const contractReducer = (state=[], action) => {
       return state;
   }
 };
+
+const middleware = applyMiddleware(thunk, logger())
+const store = createStore(contractReducer, middleware)
+
+store.dispatch((dispatch) => {
+  dispatch({type: "FETCH_CONTRACTS"})
+    axios.get("https://private-aee43-decodecatsiouct.apiary-mock.com/contracts")
+    // dispatch({type: "User was fetched"})
+        .then(function(result)  {
+          console.log(result);
+        })
+});
+
 
 export default contractReducer;
