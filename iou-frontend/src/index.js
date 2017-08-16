34944fa06fd2df965efe2616821db441d59a712a
TSirrus <tsirrus@gmail.com>
2017-08-16 03:10:38 +0000

    Cleanup of comments, added first pieces (in comments) of redux

--- iou-frontend/src/index.js
+++ iou-frontend/src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { /*Router, Route,*/ BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+
 import App from './components/App';
-// import Connection from './components/pages/Connection';
-// import Contract from './components/pages/Contract';
-// import Notification from './components/pages/Notification';
-// import Home from './components/pages/Home';
-// import Login from './components/pages/Login';
-// import SignUp from './components/pages/SignUp';
+
 
 import './index.css';
 // import registerServiceWorker from './registerServiceWorker';
 
-// const routes = (
-//     <Router history={BrowserRouter}>
-//       <Route path="/" component={Home}>
-//         <Route path="/connections" component={Connection}/>
-//         <Route path="/contracts/:id" component={Contract}/>
-//         <Route path="/notifications" component={Notification}/>
-//         <Route path="/signup" component={SignUp}/>
-//         <Route path="/login" component={Login}/>
-//       </Route>
-//     </Router>
-// );
+// let store = createStore(todoApp);
 
 ReactDOM.render(
   <BrowserRouter>
