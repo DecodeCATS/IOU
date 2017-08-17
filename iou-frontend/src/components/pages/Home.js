53e1f13d2c841a8307ff5ea2203a616e971a1a58
Tetyana Myronova <tetyanamyronova@LWCHAmyronovate.local>
2017-08-16 20:25:59 -0400

    Add contractReducer functionality

--- iou-frontend/src/components/pages/Home.js
+++ iou-frontend/src/components/pages/Home.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 // import api from '../../api';
 // import auth from '../../auth';
 import * as Connections from '../../actions/connectionActions';
-// import * as Contracts from '../../actions/contractActions';
+import * as Contracts from '../../actions/contractActions';
 import './Home.css';
 // import {browserHistory as history} from 'react-router';
 
@@ -18,18 +18,20 @@ class Home extends Component {
     
     componentWillMount() {
         this.props.dispatch(Connections.fetchConnections());
+        this.props.dispatch(Contracts.fetchContracts());
     }
 
     render() {
         // let {boards} = this.state;
         // this.props.contracts
         //console.log(`Rendering=${JSON.stringify(this.state.boards)}`);
-        console.log(this.props.connections);
+        console.log(this.props.connections, this.props.contracts);
         return (
             <div className="home">
                 <h1>Home Page!</h1>
                 <p>{this.props.location.state}</p>
                 <p>Data load status: {this.props.connections.status}</p>
+                {/*<p>Data load status: {this.props.contracts.status}</p>*/}
             </div>
         );
     }
--- iou-frontend/src/reducers/contractReducer.js
+++ iou-frontend/src/reducers/contractReducer.js
@@ -2,14 +2,25 @@
 
 //Reducer acting on the contracts
 
-
 //The reducer affecting the contract
 const contractReducer = (state=[], action) => {
   //The reducer cannot return the state directly after a transformation, or else it means that we have changed the original state (immutability)
   switch (action.type) {
+      case "FETCH_ALL_CONTRACTS": {
+          state = {...state, status: action.status, error: action.error, data: action.value.users, dataUpdated: Date()};
+        break;
+      }
+      case "FETCH_SINGLE_CONTRACT": {
+          state.data = state.filter(contracts => {
+
+              return contracts.id === action.value.contracts.id;
+          })
+          break;
+      }
     default:
-      return state;
+        break;
   }
+    return state;
 };
 
 export default contractReducer;
