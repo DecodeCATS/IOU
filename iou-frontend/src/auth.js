c2785eac8bf792bcf2750d13cfa1dc916ba026da
Tetyana Myronova <tetyanamyronova@LWCHAmyronovate.local>
2017-08-18 12:38:09 -0400

    Small changes

--- iou-frontend/src/auth.js
+++ iou-frontend/src/auth.js
@@ -216,9 +216,9 @@ var auth = {
         }
     },
 
-    getContract() {
+    getContract(contractId) {
         if(localStorage.token) {
-            return api.getContract(localStorage.token)
+            return api.getContract(localStorage.token, contractId)
                 .then(res => {
                     return res.body; //Fix to send Contracts
                 })
