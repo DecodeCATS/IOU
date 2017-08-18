09714b6a178871faf04d16c2999e2d3730ffea81
TSirrus <tsirrus@gmail.com>
2017-08-18 14:35:28 +0000

    [Payment]Basic rendering

--- iou-frontend/src/components/pages/Payment.js
+++ iou-frontend/src/components/pages/Payment.js
@@ -7,6 +7,12 @@ import './Payment.css';
 
 //This is a smart component. It is aware of the store
 class Payment extends Component {
+  render() {
+    return (
+      <div>Payment Page!
+      </div>
+    );
+  }
 }
 
 export default connect(state => ({ user: state.user, connections: state.connections }))(Payment);
