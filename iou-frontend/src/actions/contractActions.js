f189aa58c91f3677ffb7b73191ab33f010bc572f
Tetyana Myronova <tetyanamyronova@LWCHAmyronovate.local>
2017-08-18 12:21:37 -0400

    Fix data array in contractreducer

--- iou-frontend/src/actions/contractActions.js
+++ iou-frontend/src/actions/contractActions.js
@@ -2,13 +2,13 @@ import Auth from '../auth';
 
 export function fetchContracts () {
   return function (dispatch) {
-    dispatch({type: "FETCH_ALL_CONTRACTS", status: "pending", error: "", value: []});
+    dispatch({type: "FETCH_ALL_CONTRACTS", status: "pending", error: ""});
     Auth.getContracts()
     .then(res => {
       dispatch({type: "FETCH_ALL_CONTRACTS", status: "success", error: "", value: res});
     })
     .catch(err => {
-      dispatch({type: "FETCH_ALL_CONTRACTS", status: "error", error: err, value: []});
+      dispatch({type: "FETCH_ALL_CONTRACTS", status: "error", error: err});
     });
   };
 }
