bed82d16542310ea3cf81ff5ce1fc007b8466a12
TSirrus <tsirrus@gmail.com>
2017-08-20 21:50:00 +0000

    Contracts. Rendered Titles

---
+++ iou-frontend/src/components/elements/ContractCard.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './PaymentCard.css';
+
+export default class ContractCard extends Component {
+  render(){
+    let {contract} = this.props;
+    return (
+      <div className="contract">
+        <div className="title">
+          <p>{contract.title}</p>
+        </div>
+      </div>
+    );
+  }
+}
--- iou-frontend/src/components/pages/ContractV2.js
+++ iou-frontend/src/components/pages/ContractV2.js
@@ -3,14 +3,45 @@ import {connect} from 'react-redux';
 // import * as Contracts from '../../actions/contractActions';
 // import { Button, Grid, Col, Alert } from 'react-bootstrap';
 // import ReactDOM from 'react-dom'
-// import ContractHistoryCard from '../elements/ContractHistoryCard';
-import './Contract.css';
+import ContractCard from '../elements/ContractCard';
+import './ContractV2.css';
 
 
 class Contract extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detailedContractId: 0
+    };
+    this.renderList = this.renderList.bind(this);
+  }
+  
+  renderList(contract){
+    
     return (
-      <div>
+      <div key={contract.id} className="latestCards">
+        <ContractCard contract={contract} />
+      </div>
+    );
+  }
+  
+  render() {
+    let latestContracts = [];
+    if (this.props.contracts.data) {
+      latestContracts = this.props.contracts.data;//.filter(contract => contract.isLatest);
+    }
+    console.log("Latest contracts", latestContracts);
+    return (
+      <div className="pageContainer">
+        <div className="listContainer">
+          {
+            latestContracts.map(contract => this.renderList(contract))
+          }
+        </div>
+        <div className="detailContainer">
+        </div>
+        <div className="paymentContainer">
+        </div>
       </div>
     );
   }
