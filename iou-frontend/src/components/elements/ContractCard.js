5e8ad7e1c136c6b5c773884f72215f278f812bea
SSaquif <ssaquif@gmail.com>
2017-08-21 19:13:03 -0400

    contract details now partially showing

--- iou-frontend/src/components/elements/ContractCard.js
+++ iou-frontend/src/components/elements/ContractCard.js
@@ -3,12 +3,14 @@ import React, {Component} from 'react';
 import './PaymentCard.css';
 
 export default class ContractCard extends Component {
+    //onClick={this.props.click}
   render(){
-    let {contract} = this.props;
+
+    let {contract} = this.props; //now, contract = to this.props.contract
     return (
       <div className="contract">
         <div className="title">
-          <p>{contract.title}</p>
+          <p> {contract.title} </p>
         </div>
         <div className="counterparty">
         </div>
--- iou-frontend/src/components/pages/ContractV2.js
+++ iou-frontend/src/components/pages/ContractV2.js
@@ -11,9 +11,11 @@ class Contract extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      detailedContractId: 0
+      detailedContractId: 0,
+      detailedContract: []
     };
     this.renderList = this.renderList.bind(this);
+    this.processContractDetail = this.processContractDetail.bind(this);
   }
   
   renderList(contract){
@@ -25,7 +27,8 @@ class Contract extends Component {
     if (contract.payeeId === myId) {
       isPayer = false;
     }
-    
+
+    //this.props.connections comes from the store
     if (this.props.connections.data) {
       counterparties = this.props.connections.data.filter(user => {
         let flag = false;
@@ -53,21 +56,41 @@ class Contract extends Component {
         return flag;
       });
     }
+
     console.log("Counterparty size after orgs", counterparties.length);
+      //{/*contract info from the store after api call*/}
+      {/*my flag*/}
+
+      //onClick should get a CALLBACK and NOT A FUCNTION, Else you go into infinite loop
     return (
-      <div key={contract.id} className="latestCards">
+      <div key={contract.id} className="latestCards" onClick={() => this.processContractDetail(contract.id)}>
         <ContractCard
           contract={contract}
           isPayer={isPayer}
-          counterparty={counterparties[0]}
-        />
+          counterparty={counterparties[0]}        />
       </div>
     );
   }
-  
+
+  processContractDetail(contractId){
+    //Steps 1 get the data for the specific data from the contracts array
+    console.log(contractId, "what is contract id")
+
+    //console.log(this.props.contracts.data);
+
+    let selectedContract = this.props.contracts.data.filter(contracts => {
+        console.log(contracts.id, contractId, "the filter")
+      return contracts.id === contractId;
+    });
+    console.log(selectedContract, "the selected contract array")
+    this.setState({detailedContract: selectedContract})
+  }
+
   render() {
     let latestContracts = [];
+    let selectedContractData = [];
     if (this.props.contracts.data) {
+      //this.props.contracts.data is now sored in the latestContractsArray
       latestContracts = this.props.contracts.data;//.filter(contract => contract.isLatest);
     }
     // console.log("Latest contracts", latestContracts);
@@ -83,6 +106,9 @@ class Contract extends Component {
         </div>
         <div className="detailContainer">
           <h2>Contract detail:</h2>
+          {
+            this.state.detailedContract.map(contract=> this.renderList(contract))
+          }
         </div>
         <div className="paymentContainer">
           <h2>Payment detail:</h2>
@@ -91,6 +117,10 @@ class Contract extends Component {
     );
   }
 }
+//
+// {
+//     this.state.detailedContract.map(contract=> this.renderList(contract))
+// }
 
 export default connect(state => ({ 
   user: state.user, 
